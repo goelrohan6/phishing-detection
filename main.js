a2717caca4db347d76b385d76d885fdf70354f01
historypeats <iam.historypeats@gmail.com>
2016-04-21 13:58:02 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+var gmail = null;
+/*
+ * Wait for the Gmail object to be ready
+ */
+function refresh(f) {
+	if( (/in/.test(document.readyState)) || (typeof Gmail === undefined) ) {
+		setTimeout('refresh(' + f + ')', 10);
+	} else {
+    	f();
+  	}
+}
+
+var check_failure = function() {
+	var id = null,
+		raw_email = null,
+		spoof = false;
+	
+	console.log('[Phish Detect] Scanning email...');
+
+	id = window.location.hash.split('/').pop(),
+	raw_email = gmail.get.email_source(id),
+	spoof = /=fail/.test(raw_email);
+
+	if(spoof) {
+		alert('This could be a phishing email!');
+		console.log('This could be a phishing email!');
+	}
+};
+
+var main = function(){
+	console.log('[Phish Detect] Gmail object defined. Ready to protect.');
+
+	gmail = new Gmail();
+	gmail.observe.after('open_email', check_failure);
+};
+
+// Wait for for document and Gmail objects to be ready
+refresh(main);
+
