1eadf2d58031c9c7292350fb1d19ee6a167c79c5
historypeats <iam.historypeats@gmail.com>
2016-04-26 11:13:34 -0400

    Working extension with alert

--- content.js
+++ content.js
@@ -4,16 +4,10 @@ var j = document.createElement('script');
 j.src = chrome.extension.getURL('node_modules/gmail-js/node_modules/jquery/dist/jquery.min.js');
 (document.head || document.documentElement).appendChild(j);
 
-
 // Inject Gmail.js into DOM
 var g = document.createElement('script');
 g.src = chrome.extension.getURL('node_modules/gmail-js/src/gmail.js');
 (document.head || document.documentElement).appendChild(g);
-
-// Inject jquery into DOM
-var p = document.createElement('script');
-p.src = chrome.extension.getURL('node_modules/bootstrap/dist/js/bootstrap.min.js');
-(document.head || document.documentElement).appendChild(p);
 
 // Inject main.js. Main should contain the rest of the code
 var s = document.createElement('script');
